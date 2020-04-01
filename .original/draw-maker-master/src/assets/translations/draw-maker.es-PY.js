50a209788d37954fd989e81b4ce6b131a82f807d
hudaokeji <github@hudaokeji.com>
2020-04-01 11:54:06 +0900

    add original version

---
+++ .original/draw-maker-master/src/assets/translations/draw-maker.es-PY.js
@@ -0,0 +1,31 @@
+(function (owner) {
+    "use strict";
+
+    let ns = owner.drawmaker || (owner.drawmaker = {});
+
+    ns.textTranslation = {
+        "Draw Rectangle": "Dibujar Rectángulo",
+        "Draw Ellipse": "Dibujar Elipse",
+        "Draw Triangle": "Dibujar Triángulo",
+        "Draw Line": "Dibujar Linea",
+        "Draw Arrow": "Dibujar Flecha",
+        "Draw Double Arrow": "Dibujar Doble Flecha",
+        "Text Box": "Caja de Texto",
+        "Free Drawing": "Dibujo Libre",
+        "Remove": "Eliminar",
+        "Duplicate": "Duplicar",
+        "Zoom In": "Acercar Zoom",
+        "Zoom Out": "Alejar Zoom",
+        "Fullscreen": "Pantalla Completa",
+        "Settings": "Configuraciones",
+        "Fill/Text Color": "Color de Relleno/Texto",
+        "Line/Border Color": "Color de Línea/Borde",
+        "Line/Border Width": "Grosor de Línea/Borde",
+        "Line/Border Style": "Estilo de Línea/Borde",
+        "Object Opacity": "Opacidad del Objeto",
+        "Bring Forwards": "Traer al Frente",
+        "Send Backwards": "Enviar al Revés",
+        "Without Color / Transparent": "Sin Color / Transparente",
+    };
+
+})(this);
